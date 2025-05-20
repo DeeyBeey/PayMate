9a9164a8648ff25174a019aeb33ffeb75cc4158c
Dhruv Belai <dhruv_belai@yahoo.com>
2025-05-20 18:48:11 -0400

    fixed api key public leak

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,20 +1,14 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBnIfeUXX8nu4cvt6g0cb4S48DzWSgGdl8",
-  authDomain: "paymate-2fb61.firebaseapp.com",
-  projectId: "paymate-2fb61",
-  storageBucket: "paymate-2fb61.firebasestorage.app",
-  messagingSenderId: "690549200026",
-  appId: "1:690549200026:web:a853b87561beb42bcb7b23",
-  measurementId: "G-JHV8WWLSSR"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
